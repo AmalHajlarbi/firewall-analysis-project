f1673c85e2a31c21877e06f742b455441e5cba07
SamiaBenNasr <samia.bennasr@insat.ucar.tn>
2026-01-26 20:08:02 +0100

    ignore authentification for analysis endpoints

--- backend/src/analysis/analysis.controller.ts
+++ backend/src/analysis/analysis.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { AnalysisService } from './analysis.service';
 import { AnalysisFilterDto } from './dto/analysis-filter.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('analysis')
 export class AnalysisController {
   constructor(private readonly analysisService: AnalysisService) {}
 
   @Get('statistics')
+  @Public()
   async getStatistics(@Query() query: AnalysisFilterDto) {
     return this.analysisService.statistics(query);
   }
 
   @Get('anomalies')
+  @Public()
   async getAnomalies(@Query() query: AnalysisFilterDto) {
     return this.analysisService.detectAnomalies(query);
   }
--- backend/src/search/search.controller.ts
+++ backend/src/search/search.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchLogsDto } from './dto/search-logs.dto';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('logs/search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
   
   @Get()
+  @Public()
   search(@Query() query: SearchLogsDto) {
     return this.searchService.search(query);
   }
