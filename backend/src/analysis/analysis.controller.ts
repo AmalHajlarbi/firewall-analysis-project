d769f88efa02c6201fadc02b63ba459e524c0614
SamiaBenNasr <samia.bennasr@insat.ucar.tn>
2026-01-26 20:22:56 +0100

    merging commit 'frontend/dashboards_logs' into develop2

--- backend/src/analysis/analysis.controller.ts
+++ backend/src/analysis/analysis.controller.ts
@@ -8,7 +8,6 @@ export class AnalysisController {
   constructor(private readonly analysisService: AnalysisService) {}
 
   @Get('statistics')
-  async getStatistics(@Query() query: AnalysisFilterDto) {
   @Public()
   async getStatistics(@Query() query: AnalysisFilterDto) {
     return this.analysisService.statistics(query);
