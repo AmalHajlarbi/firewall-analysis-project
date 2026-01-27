24b8746866f11b77d456ba842766d04310a45141
SamiaBenNasr <samia.bennasr@insat.ucar.tn>
2026-01-27 13:50:08 +0100

    fixing data format

--- backend/src/search/search.controller.ts
+++ backend/src/search/search.controller.ts
@@ -3,13 +3,14 @@ import { SearchService } from './search.service';
 import { SearchLogsDto } from './dto/search-logs.dto';
 import { Public } from '../auth/decorators/public.decorator';
 
+
 @Controller('logs/search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
   
   @Get()
   @Public()
-  search(@Query() query: SearchLogsDto) {
+  async search(@Query() query: SearchLogsDto) {
     return this.searchService.search(query);
   }
 }
